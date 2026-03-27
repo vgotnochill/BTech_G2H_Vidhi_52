cab266a6155cbb00eb63d2251007f3ef536dc82b
vgotnochill <25121cn122@glbitm.ac.in>
2026-03-27 09:46:32 +0530

    Create thirteen.c

---
+++ thirteen.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+int main()
+{
+    int n,i,sum=0;
+    
+    printf("Enter number of elements:");
+    scanf("%d",&n);
+    
+    int arr[n];
+    
+    printf("Enter array elements:\n",n);
+    
+        for(i=0;i<n;i++){
+            scanf("%d",&arr[i]);
+        }
+        
+        for(i=0;i<n;i++){
+            sum += arr[i];
+        }
+    
+    printf("Sum of aray elements =%d\n",sum);
+
+    return 0;
+}
